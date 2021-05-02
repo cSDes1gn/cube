57caaba6959f34d480cc886d8e470295849db13e
cSDes1gn <christian911@sympatico.ca>
2021-05-01 20:09:39 -0400

    using plain document query and DOM manipulation for animation fucntions

--- cube/cube.ts
+++ cube/cube.ts
@@ -1,138 +1,71 @@
-import anime from 'animejs';
+import { nodes, Node } from './node';
 
-// get window origin
 let wo_x = window.innerWidth / 2;
 let wo_y = window.innerHeight / 2;
 const refreshRate = 1000 / 60;
 const duration = 2000;
-const spacing = 200;
-console.log("Window center: " + wo_x + ", " + wo_y );
-
-interface Node {
-    el:string,
-    x:number,
-    y:number,
-    z:number
-}
-var nodes = [];
-var nodes_next = [];
-nodes.push(
-    <Node> {
-        el: '#d1',
-        x: wo_x-spacing,
-        y: wo_y-spacing,
-        z: 0
-    },
-    <Node> {
-        el: '#d2',
-        x: wo_x,
-        y: wo_y-spacing,
-        z: 0
-    },
-    <Node> {
-        el: '#d3',
-        x: wo_x+spacing,
-        y: wo_y-spacing,
-        z: 0
-    },
-    <Node> {
-        el: '#d4',
-        x: wo_x-spacing,
-        y: wo_y,
-        z: 0
-    },
-    <Node> {
-        el: '#d5',
-        x: wo_x,
-        y: wo_y,
-        z: 0
-    },
-    <Node> {
-        el: '#d6',
-        x: wo_x+spacing,
-        y: wo_y,
-        z: 0
-    },
-    <Node> {
-        el: '#d7',
-        x: wo_x-spacing,
-        y: wo_y+spacing,
-        z: 0
-    },
-    <Node> {
-        el: '#d8',
-        x: wo_x,
-        y: wo_y+spacing,
-        z: 0
-    },
-    <Node> {
-        el: '#d9',
-        x: wo_x+spacing,
-        y: wo_y+spacing,
-        z: 0
-    },
-);
-
 
 window.onload = () => {
-    var play = document.querySelector('.play');
-    play.addEventListener('click', function(e) {
-        e.preventDefault();
-        arr.forEach((el:anime.AnimeInstance) => {
-            el.play()
-        });
-    });
-    // initializer 
+    // bind event button
+    document.querySelector('.play').addEventListener('click', anime);
+    // initialize node positions
     nodes.forEach((node:Node) => {
-        anime({
-            targets: node.el,
-            keyframes: [
-                {translateX: node.x},
-                {translateY: node.y}
-            ],
-            easing: 'easeInOutExpo',
-            duration: duration
-        });
+        var elem = document.getElementById(node.el);
+        elem.style.top = node.x + "px"; 
+        elem.style.left = node.y + "px"; 
     });
-    rotate_z(Math.PI/2);
-    let arr = []
-    for (let i = 0; i < 9; i++) {
-        var a = anime({
-            targets: nodes[i].el,
-            translateX: [ nodes[i].x, nodes_next[i][0]],
-            translateY: [ nodes[i].y, nodes_next[i][1] ],
-            easing: 'easeInOutExpo',
-            duration: duration,
-            autoplay: false
-        });
-        arr.push(a);
-    }
 };
 
-// setup dots on page
+function anime(e:any) {
+    e.preventDefault();
+    // iterate through nodes and apply transform
+    let target =(<HTMLInputElement>document.getElementById("target")).value;
+    let deg = parseInt(target);
+    nodes.forEach((node:Node) => {
+        let id = null;
+        // degree step size
+        var dss = 0.1;
+        var elem = document.getElementById(node.el);
+        id = setInterval(frame, 20);
+        function frame() {
+            if ( dss >= deg) {
+                clearInterval(id);
+            } else {
+                dss += 2;
+                // get xy transform for this node
+                let xy_t = rotate_z(deg_to_rad(dss),node.x, node.y);
+                elem.style.top = xy_t[0] + "px"; 
+                elem.style.left = xy_t[1] + "px";
+                // stateful
+                // node.x = xy_t[0];
+                // node.y = xy_t[1]; 
+            }
+        }
+    })
+}
+
+function rad_to_deg(rad:number):number {
+    return (rad * 180) / Math.PI;
+}
+
+function deg_to_rad(deg:number):number {
+    return (deg * Math.PI) / 180;
+}
 
 // perform transforms
 // x' = x * cos(B) - y * sin(B)
 // y' = y * cos(B) + x * sin(B)
-function rotate_z(theta:number) {
+function rotate_z(rss:number, x:number, y:number):[number, number] {
     // optimization
-    let sin_theta = Math.sin(theta);
-    let cos_theta = Math.cos(theta);
-    // map transform over all input points
-    nodes.forEach((node) => {
-        // subtract offset
-        node.x -= wo_x;
-        node.y -= wo_y;
-        // perform translation computation
-        let x = (node.x * cos_theta - node.y * sin_theta) + wo_x;
-        let y = (node.y * cos_theta + node.x * sin_theta) + wo_y;
-        // append offset
-        node.x += wo_x;
-        node.y += wo_y;
-        nodes_next.push([x, y])
-    })
-    console.log(nodes);
-    console.log(nodes_next);
+    let cos_theta = Math.cos(rss);
+    let sin_theta = Math.sin(rss);
+    // subtract offset
+    x -= wo_x;
+    y -= wo_y;
+    // perform translation computation
+    let x_prime = (x * cos_theta - y * sin_theta) + wo_x;
+    let y_prime = (y * cos_theta + x * sin_theta) + wo_y;
+    return [ x_prime, y_prime ]
 }
 
 
